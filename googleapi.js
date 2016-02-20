594adb3cf922664a6df815862eca1ce4b75221b6
Geordyn Ader <Geordyn Ader>
2016-02-19 17:45:45 -0700

    file setup

---
+++ googleapi.js
@@ -0,0 +1,15 @@
+// <!-- GOOGLE MAPS API IN INDEX.HTML-->
+  //  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtAP_a3NvJ5NdIfNKXs_wgXaept708HFY&libraries=places"></script>
+//geordyns API Key
+// <address-search address-data="newStudentObj.addressData"></address-search>
+
+
+
+// if ($scope.newUserObj.hasOwnProperty('addressData')) {
+//       // get district for new user based on lat/lon
+//       var lat = $scope.newUserObj.addressData.lat;
+//       var lng = $scope.newUserObj.addressData.lng;
+//
+//
+//     }
+// front end structure
