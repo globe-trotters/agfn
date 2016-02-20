72bc525066231941b7c0b21d81a1debc1e0f24ed
Geordyn Ader <Geordyn Ader>
2016-02-19 17:55:47 -0700

    trying to set up files

---
+++ public/js/app.js
@@ -0,0 +1,55 @@
+angular.module('myApp', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+
+  $stateProvider
+  ///////USER VIEWS///////
+    .state('map', {
+      url: '/map',
+      templateUrl: './js/views/mapView.html',
+      // controller: 'mapCtrl',
+    })
+    .state('finance', {
+      url: '/finance',
+      templateUrl: './js/views/financeView.html',
+      // controller: 'financeCtrl'
+    })
+  .state('admin', {
+    url: '/admin',
+    templateUrl: './js/views/adminView.html',
+    // controller: 'adminCtrl',
+
+    // resolve: {
+    //   user: function($state, loginService) {
+    //     return loginService.getCurrentUser()
+    //       .then(function(res) {
+    //         if (res.status != 200) {
+    //           console.log('brack');
+    //           alert('Unauthorized');
+    //           $state.go('home');
+    //
+    //         } else {
+    //           return res.data;
+    //         }
+    //       }, function(err) {
+    //         console.log('brack');
+    //         alert('Unauthorized');
+    //         $state.go('home');
+    //
+    //       });
+    //   }
+    // }
+  });
+  $urlRouterProvider.otherwise('/map');
+
+});
+
+// .directive('navDir', function() {
+//     return {
+//       templateUrl: './dirs/navDir.html',
+//       restrict: 'E', //directive can only be used as a full element
+//       //restrict: 'A' restricts attributes
+//       //can also use both 'EA'
+//     };
+//   });
