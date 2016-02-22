9e1dbbba48bf46076fee2c8d1857a9e7ec16e04b
Adam <miknevinas@gmail.com>
2016-02-22 16:28:29 -0700

    graph commit

---
+++ public/js/controllers/housingGraphCtrl.js
@@ -0,0 +1,110 @@
+angular.module("myApp").controller("housingLivingCtrl", function($scope, financeSvc) {
+    
+    showGraph();
+    
+    function showGraph() {
+        
+        var margin = {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 40
+        },
+            width = 1000 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+        
+        var x0 = d3.scale.ordinal()
+            .rangeRoundsBands([0, width], .1);
+        var x1 = d3.scale.ordinal();
+        
+        var y = d3.scale.linear()
+            .range([height, 0]);
+        
+        var xAxis = d3.svg.axis()
+            .scale(x0)
+            .orient('bottom');
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient('left')
+            .tickFormat(d3.format('.2s'));
+        
+        var svg = d3.select('#housingGraph').append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        
+        d3.json('graphData.json', function(err, data) {
+            if (err) {
+                throw err;
+            }
+            var places = d3.keys(data[0]).filter(function(key) {
+                return key !== 'place';
+            });
+            
+            data.forEach(function(d) {
+                d.expenses = places.map(function(place) {
+                    return {place: place, value: +d[place]};
+                });
+            });
+            
+            x0.domain(data.map(function(d) {
+                return d.place;
+            }))
+            x1.domain(places).rangeRoundBands([0, x0.rangeBand()]);
+            y.domain([0, d3.max(data), function(d) {
+                return d3.max(d.expenses, function(d) {
+                    return d.value;
+                });
+            }]);
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Population");
+
+            var state = svg.selectAll(".state")
+            .data(data)
+            .enter().append("g")
+            .attr("class", "state")
+            .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
+
+            state.selectAll("rect")
+                .data(function(d) { return d.expenses; })
+                .enter().append("rect")
+                .attr("width", x1.rangeBand())
+                .attr("x", function(d) { return x1(d.name); })
+                .attr("y", function(d) { return y(d.value); })
+                .attr("height", function(d) { return height - y(d.value); })
+                .style("fill", function(d) { return color(d.name); });
+
+            var legend = svg.selectAll(".legend")
+            .data(places.slice().reverse())
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+            legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", color);
+
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(function(d) { return d; });
+        })
+    }
+})
