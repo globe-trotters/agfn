2fd70f0e843f4bb7f4d2157bd1644744ffd46782
Fred Nyambati <Fred@Admins-MacBook-Pro-3.local>
2016-02-22 17:22:51 -0700

    financials work front to back

--- public/js/controllers/financeCtrl.js
+++ public/js/controllers/financeCtrl.js
@@ -3,30 +3,24 @@ angular.module('myApp')
 
     $scope.getFinance = function() {
       financeService.getFinance().then(function(res) {
-        console.log(res);
-        $scope.finance = res;
+        $scope.finances = res;
       });
     };
 
     $scope.addFinance = function(newFinance) {
       financeService.addFinance(newFinance).then(function(res) {
-        console.log(res);
         $scope.getFinance();
       });
     };
 
     $scope.editFinance = function(editFinance) {
-      console.log(editFinance);
       financeService.editFinance(editFinance).then(function(res) {
-        console.log(res);
         $scope.getFinance();
       });
     };
 
     $scope.deleteFinance = function(id) {
-      console.log(id);
       financeService.deleteFinance(id).then(function(res) {
-        console.log(res);
         $scope.getFinance();
       });
     };
