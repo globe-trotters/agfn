c3f2cb96e5b7a837815b52132ac31b1f5a8a18d1
Nick Marucci <npm1514@gmail.com>
2016-02-29 16:38:11 -0700

    added globe to group

--- public/js/app.js
+++ public/js/app.js
@@ -130,4 +130,47 @@ angular.module('myApp', ['ui.router'])
         },
         controller: 'employmentGraphCtrl'
     };
+})
+.directive('body', function() {
+ return {
+   restrict: 'E',
+   scope: {
+     zoomin: '=',
+     zoomout: '='
+   },
+   controller: 'mainCtrl'
+ };
+})
+.directive('earth', function() {
+ return {
+   restrict: 'E',
+   scope: {
+     zoomin: '=',
+     zoomout: '=',
+     cohortupdate:'='
+   },
+   controller: 'earthCtrl'
+ };
+})
+.directive('bars', function() {
+ return {
+  restrict: 'E',
+  scope: {
+    data:'=',
+    cohortupdate:'=',
+  },
+  controller: 'barsCtrl'
+ };
+})
+.directive('scrollbar', function() {
+ return {
+   restrict: 'E',
+   scope: {
+     cohortupdate: '=',
+     arcit: '&',
+     scrolldate: '=',
+     play: '='
+   },
+   controller: 'scrollbarCtrl'
+ };
 });
